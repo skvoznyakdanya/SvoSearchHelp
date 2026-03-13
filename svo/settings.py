4b4a0a02e1d06bebfb8f39a3fdb3cdb808e2dfbd
Ivan <ivan@example.com>
2026-03-13 03:14:57 +0300

    fix database

--- svo/settings.py
+++ svo/settings.py
@@ -52,11 +52,11 @@ WSGI_APPLICATION = 'svo.wsgi.application'
 
 # База данных (Postgres на Render)
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL')
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
-
 # Пароли
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
